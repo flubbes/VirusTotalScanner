22b3381c95b6ffd5ef50bda19402a59a14eb0b44
Marvin Karaschewski <marvin@kabesoft.de>
2015-03-11 14:34:25 +0100

    Added basic SubsystemMonitoring

---
+++ VirusTotalScanner/Monitoring/ISubsystemMonitoringUnit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VirusTotalScanner.Monitoring
+{
+    interface ISubsystemMonitoringUnit
+    {
+        event EventHandler NewAlert;
+        bool IsRunning { get; set; }
+        void Start();
+        void Stop();
+    }
+}
