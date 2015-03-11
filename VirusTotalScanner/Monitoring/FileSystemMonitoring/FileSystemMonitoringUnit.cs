527b742d66007fb21ceb9d93d9be5d25d6c2cfba
Marvin Karaschewski <marvin@kabesoft.de>
2015-03-11 14:59:01 +0100

    Changed the NewAlertEventHandler

--- VirusTotalScanner/Monitoring/FileSystemMonitoring/FileSystemMonitoringUnit.cs
+++ VirusTotalScanner/Monitoring/FileSystemMonitoring/FileSystemMonitoringUnit.cs
@@ -4,7 +4,7 @@ namespace VirusTotalScanner.Monitoring.FileSystemMonitoring
 {
     class FileSystemMonitoringUnit : ISubsystemMonitoringUnit
     {
-        public event EventHandler NewAlert;
+        public event NewAlertEventHandler NewAlert;
         public bool IsRunning { get; set; }
 
         public void Start()
