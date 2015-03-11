e83e701ab76624cd3905733afbad58ffb3f1b159
Fabian Berg <info@fabian-berg.de>
2015-03-11 14:29:33 +0100

    added AlertBehavior and some properties

--- VirusTotalScanner/Monitoring/MonitoringUnitController.cs
+++ VirusTotalScanner/Monitoring/MonitoringUnitController.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Drawing.Text;
 
 namespace VirusTotalScanner.Monitoring
 {
-    class MonitoringUnitController
+    public class MonitoringUnitController
     {
+        public MonitoringUnitController()
+        {
+            Units = new List<ISubsystemMonitoringUnit>();
+            AlertBehaviors = new List<IAlertBehavior>();
+        }
+
+        public List<ISubsystemMonitoringUnit> Units { get; private set; }
+
+        public List<AlertBehaviors> AlertBehaviors
+        {
+            get;
+            private set;
+        } 
     }
 }
