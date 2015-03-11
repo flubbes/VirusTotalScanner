5198afc0f2481b2485f72fa4d7b51e67efe79766
Fabian Berg <info@fabian-berg.de>
2015-03-11 16:13:35 +0100

    added monitorController functionality and started to implement the virusScanner

---
+++ VirusTotalScanner/Monitoring/AlertBehaviors/NoMatchingBehaviorBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using VirusTotalScanner.Monitoring.Alerts;
+
+namespace VirusTotalScanner.Monitoring.AlertBehaviors
+{
+    public class NoMatchingBehaviorBehavior : IAlertBehavior
+    {
+        public void HandleAlert(IAlert alert)
+        {
+            MessageBox.Show("An unhandled alert was found: " + alert);
+        }
+
+        public bool IsMatching(IAlert alert)
+        {
+            return true;
+        }
+    }
+}
---
+++ VirusTotalScanner/Scanning/Local/VirusDefinition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace VirusTotalScanner.Scanning.Local
+{
+    public class VirusDefinition
+    {
+        public string Hash { get; set; }
+        public string FileName { get; set; }
+        public int UpVotes { get; set; }
+        public int DownVotes { get; set; }
+        public List<ScanResult> ScanResults { get; set; }
+    }
+
+    public class ScanResult
+    {
+        public string AntiVirus { get; set; }
+        public string Definition { get; set; }
+        public string Update { get; set; }
+    }
+}
